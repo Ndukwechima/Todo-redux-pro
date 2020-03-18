0bd4f5ed83b0be0e0a3e1c8a6c54835953544bd5
Barnabas <Barnabas-chimaobi>
2020-03-18 23:55:58 +0100

    First commit

---
+++ src/Router/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+
+const About = () => {
+     return(
+          <div className='container'>
+               <h4 className='center'>About</h4>
+               <p>loreuhff nfffdndjfjdfndjdjdf njfkd nddfj djdjd hdhdhdh ddddd dhdhdhheb dgduigr ggdh dfhjdhy huf dhjj dhhb hhdgd gwgg hjfejfe bjhjhjff iekfeeje jjifee jkkfkkfe fhhefh hhhgfhdf fhfdghdhhdfd hfdhdhdhdf hhge eeuiuti itiiit</p>
+          </div>
+     ) 
+}
+export default Rainbow(About);     
---
+++ src/Router/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const Contact = (props) => {
+  setTimeout(() => {
+       props.history.push('/about')
+  }, 2000);
+     return(
+          <div className='container'>
+               <h4 className='center'>Contact</h4>
+               <p>loreuhff nfffdndjfjdfndjdjdf njfkd nddfjdf djdjd djmdjd jmdfjd djdjd nhddf yeieir bjfitr rigrogiorir ggwgwgw gjrjrjfrjf gwggw hhfffe furr jrgg  djhhj jfjdhd ioropfp feepeke ejh hhh hffbdjfjfd fjjfjfdf jf jfjfj ddfdfb ehfh og bkbfkf fjjfff kkhfghbvf</p>
+          </div>
+     )
+}
+export default Contact;  
